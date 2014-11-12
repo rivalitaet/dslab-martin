6559b897d62085084224caa2d00ebff8228369f9
Martin <martin.st@wolke7.net>
2014-11-12 16:18:50 +0100

    Handle NumberFormatExceptions in Calculations

--- src/main/java/controller/computation/Computation.java
+++ src/main/java/controller/computation/Computation.java
@@ -8,32 +8,34 @@ public abstract class Computation {
 
 	public abstract int getPrice();
 
-	public static Computation compute(String calculation, Calculator calc) throws CalculationException {
+	public static Computation getComputation(String calculation, Calculator calc) throws CalculationException {
 		String[] parts = calculation.split("\\s");
 
-		if (parts.length < 3) {
-			throw new CalculationException("Not a valid calculation (to few operands/operations)");
-		}
-
-		if (parts.length % 2 == 0) {
-			throw new CalculationException("Not a valid calculation (Wrong number of operands/operations)");
-		}
-
-		Computation lastComputation = new Number(Integer.parseInt(parts[0]));
-
-		for (int i = 1; i < parts.length; i++) {
-			if (i % 2 == 0) {
-				Number current = new Number(Integer.parseInt(parts[i]));
-				String op = parts[i - 1];
-				lastComputation = new Operation(lastComputation, current, op, calc);
+		try {
+			if (parts.length == 0) {
+				throw new CalculationException("Not a valid calculation (to few operands/operations)");
+			} else if (parts.length == 1) {
+				return new Number(Integer.parseInt(parts[0]));
 			}
+
+			if (parts.length % 2 == 0) {
+				throw new CalculationException("Not a valid calculation (Wrong number of operands/operations)");
+			}
+
+			Computation lastComputation = new Number(Integer.parseInt(parts[0]));
+
+			for (int i = 1; i < parts.length; i++) {
+				if (i % 2 == 0) {
+					Number current = new Number(Integer.parseInt(parts[i]));
+					String op = parts[i - 1];
+					lastComputation = new Operation(lastComputation, current, op, calc);
+				}
+			}
+
+			return lastComputation;
+		} catch (NumberFormatException e) {
+			throw new CalculationException("Not a valid number: " + e.getMessage());
 		}
-
-		System.out.println(lastComputation);
-		System.out.println(lastComputation.getResult());
-		System.out.println(lastComputation.getPrice());
-
-		return lastComputation;
 	}
 
 	public static void main(String[] args) {
@@ -44,7 +46,7 @@ public abstract class Computation {
 		while (sc.hasNextLine()) {
 			String line = sc.nextLine();
 			try {
-				Computation computation = compute(line, calc);
+				Computation computation = getComputation(line, calc);
 				int res = computation.getResult();
 				System.out.println("result: " + res);
 			} catch (CalculationException e) {
